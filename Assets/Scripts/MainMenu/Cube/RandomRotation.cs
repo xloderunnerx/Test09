1b67b759f30fa4bfc3af8c9aae1768205f5a48c2
xloderunnerx <xloderunnerx@gmail.com>
2019-09-19 18:55:41 +0300

    Main menu cube.

---
+++ Assets/Scripts/MainMenu/Cube/RandomRotation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MainMenu.Cube
+{
+    public class RandomRotation : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+        [SerializeField] private float _rotationSpeed;
+        [SerializeField] private float _addTorqueFrequency;
+        private float _addTorqueFrequencyCounter;
+
+        void Start()
+        {
+            _addTorqueFrequencyCounter = 0;
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            RotateRandomly();
+        }
+
+        void RotateRandomly()
+        {
+            if (!AddTorqueTimer())
+                return;
+
+            _rigidbody.AddTorque(new Vector3(Random.Range(0.0f, 1.0f) * _rotationSpeed, Random.Range(0.0f, 1.0f) * _rotationSpeed, Random.Range(0.0f, 1.0f) * _rotationSpeed), ForceMode.Impulse);
+        }
+
+        private bool AddTorqueTimer()
+        {
+            _addTorqueFrequencyCounter -= Time.deltaTime;
+
+            if (_addTorqueFrequencyCounter <= 0)
+            {
+                _addTorqueFrequencyCounter = _addTorqueFrequency;
+                return true;
+            }
+            return false;
+        }
+    }
+}
